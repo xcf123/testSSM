70440cc4f975d8a427bb85d0d1e43ca6990aa8ca
F谢 1 <710796720@.com>
2017-12-27 19:08:13 +0800

    添加person类

---
+++ src/main/java/com/model/Person.java
@@ -0,0 +1,22 @@
+package com.model;
+
+public class Person {
+    private String p_name;
+    private  String p_password;
+
+    public String getP_name() {
+        return p_name;
+    }
+
+    public void setP_name(String p_name) {
+        this.p_name = p_name;
+    }
+
+    public String getP_password() {
+        return p_password;
+    }
+
+    public void setP_password(String p_password) {
+        this.p_password = p_password;
+    }
+}
